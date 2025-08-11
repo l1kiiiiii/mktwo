5f4c2e69b42b0804f0357e0c457f321829110c6e
l1kiiiiii <ailikhith2116@gmail.com>
2025-08-11 19:44:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mktwo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mktwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.mktwo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+    /**
+      * A native method that is implemented by the 'mktwo' native library,
+      * which is packaged with this application.
+      */
+     external fun stringFromJNI(): String
+
+     companion object {
+         // Used to load the 'mktwo' library on application startup.
+         init {
+             System.loadLibrary("mktwo")
+         }
+     }
+}
